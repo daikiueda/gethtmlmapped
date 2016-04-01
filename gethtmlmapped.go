8a0c47960a0938ca752a8121af89f8fed235b76e
Daiki UEDA <ueda@p-17-pc.local>
2016-04-01 16:58:09 +0900

    ファイル数計上のみとフェッチ機能を追加

--- gethtmlmapped.go
+++ gethtmlmapped.go
@@ -1,23 +1,51 @@
 package main
 
 import (
+	"bufio"
 	"encoding/xml"
 	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"path"
+	"regexp"
+	"strings"
+	"time"
+
+	flags "github.com/jessevdk/go-flags"
 )
+
+type Options struct {
+	Count bool `short:"c" long:"count" description:"Only count listed HTML files (Disabled by default)"`
+	Fetch bool `short:"f" long:"fetch" description:"Fetch listed HTML files (Disabled by default)"`
+	Wait  int  `short:"w" long:"wait" default:"200" description:"Fetch duration (by milli seconds)"`
+}
+
+var opts Options
 
 type Sitemap struct {
 	Page []struct {
-		Url string `xml:"loc"`
+		Url     string `xml:"loc"`
+		Lastmod string `xml:"lastmod"`
 	} `xml:"url"`
 }
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Fprintln(os.Stderr, "URL required")
+	start := time.Now()
+
+	parser := flags.NewParser(&opts, flags.Default)
+	parser.Name = "gethtmlmapped"
+	parser.Usage = "http://example.com/sitemap.xml [OPTIONS]"
+
+	args, err := parser.Parse()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if len(args) == 0 {
+		parser.WriteHelp(os.Stdout)
 		os.Exit(1)
 	}
 
@@ -28,16 +56,74 @@ func main() {
 	}
 
 	contents := Sitemap{}
-	err = xml.Unmarshal([]byte(sitemap), &contents)
-	if err != nil {
+	if err := xml.Unmarshal([]byte(sitemap), &contents); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
+	if opts.Count {
+		fmt.Println(len(contents.Page))
+		os.Exit(0)
+	}
+
+	repl := regexp.MustCompile(`^https?://`)
+
+	topdir := strings.Split(repl.ReplaceAllString(contents.Page[0].Url, ""), "/")[0]
+	if _, err := os.Stat(topdir); opts.Fetch && err == nil {
+		fmt.Fprintln(os.Stderr, topdir+" directory is already exists")
+		os.Exit(1)
+	}
+
+	wait := time.Duration(opts.Wait) * time.Millisecond
+	succeed := []string{}
+	failed := []string{}
 	for _, page := range contents.Page {
 		fmt.Println(page.Url)
+
+		if !opts.Fetch {
+			continue
+		}
+
+		filepath := repl.ReplaceAllString(page.Url, "")
+		dir, _ := path.Split(filepath)
+
+		content, err := httpGet(page.Url)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			failed = append(failed, page.Url)
+			continue
+		}
+
+		if _, err := os.Stat(dir); err != nil {
+			if err := os.MkdirAll(dir, 0777); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				failed = append(failed, page.Url)
+				continue
+			}
+		}
+
+		fpw, err := os.Create(filepath)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			failed = append(failed, page.Url)
+			continue
+		}
+
+		w := bufio.NewWriter(fpw)
+		fmt.Fprint(w, content)
+		w.Flush()
+		fpw.Close()
+
+		succeed = append(succeed, page.Url)
+		time.Sleep(wait)
 	}
-	//fmt.Println(len(contents.Page))
+
+	if opts.Fetch {
+		end := time.Now()
+		fmt.Println("succeed : " + string(len(succeed)))
+		fmt.Println("failed : " + string(len(failed)))
+		fmt.Printf("%f sec.\n", (end.Sub(start)).Seconds())
+	}
 }
 
 func httpGet(url string) (string, error) {
